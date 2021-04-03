7b0e516266b9508d8d25124fe0e46a89728ed459
Alexandr Stepaniuk <Akeley1994@gmail.com>
2021-04-03 15:41:03 +0300

    updatetypes

--- dist/client/index.d.ts
+++ dist/client/index.d.ts
@@ -1,4 +1,4 @@
 import AuthSDK from './api';
 export * as consts from './common';
-export type { IUser } from './common/types';
+export type { IUser, IServerUser, TLoginBody } from './common/types';
 export default AuthSDK;
--- src/client/index.ts
+++ src/client/index.ts
@@ -1,6 +1,6 @@
 import AuthSDK from './api';
 
 export * as consts from './common';
-export type { IUser } from './common/types';
+export type { IUser, IServerUser, TLoginBody } from './common/types';
 
 export default AuthSDK;
